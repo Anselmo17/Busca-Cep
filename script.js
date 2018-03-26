05b231995effc37109c07df07cd4bad7092e2e67
Anselmo <Anselmo.inftec17@gmail.com>
2018-03-26 07:57:57 -0300

    devolvendo valores dos campos no HTML

--- script.js
+++ script.js
@@ -2,15 +2,41 @@
 
 let botao = document.querySelector('#busca');
 
+
+//botao que aciona um evento
 botao.addEventListener( 'click', function(){
 
+    //capturando valor do cep
     let cep = document.querySelector('#cep').value;
 
+    //pesquisando o cep digitado
     let api = `https://viacep.com.br/ws/${cep}/json/`;
 
     //estacia o objeto do tipo request
     let request = new XMLHttpRequest();
     request.open('GET' , api);
+
+    request.onload = function(){
+
+        //pegando a resposta e transformando em objeto
+        let resposta = JSON.parse(request.responseText);
+
+        //devolvendo no html
+        let cep = document.querySelector('#cep');
+        cep.innerHTML = resposta.cep;
+
+        let local = document.querySelector('#logradouro');
+        local.innerHTML = resposta.logradouro;
+
+        let bairro = document.querySelector('#bairro');
+        bairro.innerHTML = resposta.bairro;
+
+        let uf = document.querySelector('#uf');
+      uf.innerHTML = resposta.uf;
+
+    };
+
+
 
     //envia a requisição
     request.send();
